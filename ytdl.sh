30c24747a1e761c904e30ed94e14c1c6e48160a2
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-09 12:35:12 +0530

    fix

--- ytdl.sh
+++ ytdl.sh
@@ -234,6 +234,54 @@ videoUrlData() {
   echo -e "$info <videoFormatCode>+<audioFormatCode>: (ie.: 299+251)\n"
 }
 
+# Confirmation prompt function
+confirmPrompt() {
+  Prompt=${1}
+  local -n prompt_buttons=$2
+  Selected=${3:-0}  # :- set value as 0 if unset
+  maxLen=50
+  
+  # breaks long prompts into multiple lines (50 characters per line)
+  lines=()  # empty array
+  while [ -n "$Prompt" ]; do
+    lines+=("${Prompt:0:$maxLen}")  # take first 50 characters from $Prompt starting at index 0
+    Prompt="${Prompt:$maxLen}"  # removes first 50 characters from $Prompt by starting at 50 to 0
+  done
+  
+  # print all-lines except last-line
+  last_line_index=$(( ${#lines[@]} - 1 ))  # ${#lines[@]} = number of elements in lines array
+  for (( i=0; i<last_line_index; i++ )); do
+    echo -e "${lines[i]}"
+  done
+  last_line="${lines[$last_line_index]}"
+  
+  echo -ne '\033[?25l'  # Hide cursor
+  while true; do
+    show_prompt() {
+      echo -ne "\r\033[K"  # n=noNewLine r=returnCursorToStartOfLine \033[K=clearLine
+      echo -ne "$last_line "
+      [ $Selected -eq 0 ] && echo -ne "${whiteBG}➤ ${prompt_buttons[0]} $Reset   ${prompt_buttons[1]}" || echo -ne "  ${prompt_buttons[0]}  ${whiteBG}➤ ${prompt_buttons[1]} $Reset"  # highlight selected bt with white bg
+    }; show_prompt
+
+    read -rsn1 key
+    case $key in
+      $'\E')
+      # /bin/bash -c 'read -r -p "Type any ESC key: " input && printf "You Entered: %q\n" "$input"'  # q=safelyQuoted
+        read -rsn2 -t 0.1 key2  # -r=readRawInput -s=silent(noOutput) -t=timeout -n2=readTwoChar | waits upto 0.1s=100ms to read key 
+        case $key2 in 
+          '[C') Selected=1 ;;  # right arrow key
+          '[D') Selected=0 ;;  # left arrow key
+        esac
+        ;;
+      [Yy]*) Selected=0; show_prompt; break ;;
+      [Nn]*) Selected=1; show_prompt; break ;;
+      "") break ;;  # Enter key
+    esac
+  done
+  echo -e '\033[?25h' # Show cursor
+  return $Selected  # return Selected int index from this fun
+}
+
 # --- Download YouTube Videos using yt-dlp ---
 dl() {
     printf '\033[2J\033[3J\033[H'  # clear Terminal
@@ -248,7 +296,7 @@ dl() {
     if echo "$url 2>&1" | grep -qF "playlist"; then
       total=$(yt-dlp --flat-playlist --get-title "$url" 2>/dev/null | awk 'END{print NR}')
       echo -e "$info The entered URLs contain a playlist with a total of $total videos.\n"
-      read -r -p "Download [E]ntire playlist or [S]elect videos? (E/S): " choice
+      buttons=("<Entire>" "<Select>"); confirmPrompt "Download Entire playlist or Select videos?" "buttons" && choice=Entire || choice=Select
       echo -e "\n\n"  
         if [[ "$choice" =~ [Ss] ]]; then
             echo -e "$info List of Playlist items: \n\n"
@@ -610,8 +658,7 @@ dl() {
             ;;
           [Uu][Nn]*)
             # Prompt for user choice on uinstallation of python and yt-dlp
-            echo "${YELLOW}Do you want to uninstall ytdl? [Y/n]${RESET}"
-            read -r -p "Select: " opt
+            buttons=("<Yes>" "<No>"); confirmPrompt "Do you want to uninstall ytdl?" "buttons" "1" && opt=Yes || opt=No
             case $opt in
               y*|Y*)
                 formulaeUninstall "python"  # python uninstall
@@ -626,8 +673,7 @@ dl() {
                 formulaeUninstall "youtube-music"  # youtube-music uninstall
                 rm -f "$HOME/.ytdl.sh"; rm -f /usr/local/bin/ytdl           
                 ;;
-              n*|N*) echo -e "$notice ytdl uninstall skipped.";sleep 1 ;;
-              *) echo -e "$info Invalid choice. ytdl uninstall skipped."; sleep 2 ;;
+              n*|N*) echo -e "$notice ytdl uninstall skipped!";sleep 1 ;;
             esac
             ;;
           [Qq]*)
